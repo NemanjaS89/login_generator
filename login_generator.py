b9deba7b8b85a14466b92d9242e74dea4d94f830
NemanjaS89 <simic.nemanja1989@gmail.com>
2019-10-25 23:58:03 +0200

    final

--- login_generator.py
+++ login_generator.py
@@ -17,7 +17,6 @@ class User:
         self.password = password
 
 
-for i in range (10):
+for i in range (1, 10):
     user_i = User(random.choice(names) + '.' + random.choice(surnames) + str(random.randrange(100)) + random.choice(domain_list), ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation) for i in range (10)))
-    print(user_i.email)
-    print(user_i.password)
+    print('User ' + str(i) + ' generated email: ' + user_i.email + '\nUser ' + str(i) + ' generated password: ' + user_i.password)
